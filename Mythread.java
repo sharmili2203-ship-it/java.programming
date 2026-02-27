ad6989adc9a7fc807e92ac5a0f7f2dabd655ed80
sharmili2203-ship-it <sharmili2203@gmail.com>
2026-02-27 17:30:26 +0530

    Add files via upload

---
+++ Mythread.java
@@ -0,0 +1,9 @@
+class Mythread extends Thread{
+  public void run(){
+    System.out.println("Thread is running");
+  }
+  public static void  main (String args[]){
+    Mythread t=new Mythread();
+    t.start();
+  }
+}
